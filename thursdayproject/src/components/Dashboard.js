d203324fb5ff94320025ef0a2374d46ab204aca9
eoinlynchcodes <eoin95lynch@gmail.com>
2020-02-13 13:52:15 +0000

    State is being passed from Dashboard to Display components.

--- thursdayproject/src/components/Dashboard.js
+++ thursdayproject/src/components/Dashboard.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import Display from './Display';
 
 export default class Dashboard extends React.Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
             balls: 0,
             strikes: 0,
         }
     }
 
-    onStrike = (event) => {    
+    onStrike = () => {    
         this.setState({
             strikes: this.state.strikes + 1
         }); 
@@ -22,16 +23,14 @@ export default class Dashboard extends React.Component{
         }
     }
 
-    onBall = (event) => {
-        this.setState({
+    onBall = () => {
+            this.setState({
             balls: this.state.balls + 1
         });
-        console.log(this.state.balls);
         if(this.state.balls === 4){
             this.setState({
                 balls: this.state.balls = 0
             });
-            console.log(this.state.balls);
         }
     }
 
@@ -47,9 +46,14 @@ export default class Dashboard extends React.Component{
         return(
             <div>
                 <button onClick={this.onStrike}>Strike</button>
+                <h1> {this.state.strikes} </h1>
                 <button onClick={this.onBall}>Ball</button>
+                <h1> {this.state.balls}</h1>
                 <button onClick={this.onFoul}>Foul</button>
                 <button onClick={this.onHit}>Hit</button>
+        
+                <Display balls={this.state.balls} strikes={this.state.strikes}/>
+
             </div>
         )
     }
